f932c7c22b3c4e7287a0c7fd4385c40464d2f049
Brice Copy <500789+bcopy@users.noreply.github.com>
2025-11-12 16:36:26 +0100

    Remove autoSlide

--- modules/aframe-scene/src/reveal-presentation.js
+++ modules/aframe-scene/src/reveal-presentation.js
@@ -6,30 +6,8 @@ import 'reveal.js/dist/theme/white.css';
 
 Reveal.initialize({
     plugins: [ Markdown ],
-    // width: 1920,
-    // height: 1080,
-    // margin: 0.1,
     progress: false,
     controls: true,
     autoSlideMethod: "right",
-    transition: "none",
-    autoSlide: 4000,
-    // loop: true
-  }).then(() => {
-    // $(document).ready(
-    //     function(){
-    //         // $('body').on('submit','form',function() {
-    //         //     var sessionCode = $('#session_code')[0].value;
-    //         //     $("section[data-microsquad]").each(
-    //         //         function(){
-    //         //             var iframeUrl = new URL( $(this).data("background-iframe") );
-    //         //             iframeUrl.searchParams.set("sc", sessionCode);
-    //         //             $(this).data("background-iframe",iframeUrl.href);
-    //         //             console.log("Now "+$(this).data("background-iframe"));
-    //         //         }
-    //         //     );
-    //         //     return false;
-    //         // });
-    //     } 
-    // );
-});
+    transition: "none"
+  });
