7ea842eca79b86cea63662daed38b3a853735ad6
Brice Copy <500789+bcopy@users.noreply.github.com>
2026-02-09 17:49:21 +0100

    Fix CSS imports

--- modules/aframe-scene/src/presentation.js
+++ modules/aframe-scene/src/presentation.js
@@ -1,15 +1,23 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+
+// We import the CSS in JS, but Bun will bundle it into a separate CSS file.
+// Ensure the HTML has <link rel="stylesheet" href="presentation.css">
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/white.css';
 
+const deck = new Reveal({
+  plugins: [Markdown],
+  // height: 1080,
+  // width: 1920,
+  progress: false,
+  controls: true,
+  autoSlideMethod: "right",
+  transition: "none",
+});
 
-Reveal.initialize({
-    plugins: [ Markdown ],
-    // height: 1080,
-    // width: 1920,
-    progress: false,
-    controls: true,
-    autoSlideMethod: "right",
-    transition: "none"
-  });
+deck.initialize().then(() => {
+    console.log("Reveal.js initialized successfully");
+}).catch((err) => {
+    console.error("Reveal.js initialization failed", err);
+});
